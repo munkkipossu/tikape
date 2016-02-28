102a78ae81d8bdb32f3578a682d8b7a9123df785
Teemu Laakso <teemu.o.laakso@helsinki.fi>
2016-02-28 09:50:25 +0200

    Javaprojekti luotu, testi db otettu kayttoon

---
+++ harjoitustyo/src/main/java/tikape/harjoitustyo/TestiMain.java
@@ -0,0 +1,20 @@
+package tikape.harjoitustyo;
+
+import java.sql.*;
+
+public class TestiMain {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:testi.db");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT 1");
+
+        if(resultSet.next()) {
+            System.out.println("Hei tietokantamaailma!");
+        } else {
+            System.out.println("Yhteyden muodostaminen epäonnistui.");
+        }
+    }
+}
