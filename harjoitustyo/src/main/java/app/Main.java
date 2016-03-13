bf51598538d976eb4a09855448a112e0682cfebb
Teemu Laakso <teemu.o.laakso@helsinki.fi>
2016-03-13 18:13:26 +0200

    procfile main

--- harjoitustyo/src/main/java/app/Main.java
+++ harjoitustyo/src/main/java/app/Main.java
@@ -12,7 +12,7 @@ import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-public class TestiMain {
+public class Main {
 
     public static void main(String[] args) throws Exception {
         
